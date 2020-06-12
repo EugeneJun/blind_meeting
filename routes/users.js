3315934f6e5f0ab98e65ae33907fbda7ff2390fd
eugeneJ <wjsghdejr12@korea.ac.kr>
2020-06-13 05:49:30 +0900

    fix

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const DB = mongoose.createConnection('mongodb:49.247.128.56:27017/meeting', {useNewUrlParser: true, useUnifiedTopology: true });
+const DB = mongoose.createConnection('mongodb://49.247.128.56:27017/meeting', {useNewUrlParser: true, useUnifiedTopology: true });
 /* GET users listing. */
 router.post('/register', async function(req, res, next) {
   console.log(req.body);
   let exist_user = await DB.collection('users').findOne({id: req.body.id});
+  console.log(exist_user);
   if(exist_user){
     res.send({ok: 0, msg: "같은 아이디를 가진 유저가 이미 존재합니다"});
   } else {
